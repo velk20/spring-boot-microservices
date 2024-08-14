05131d577f25472a65488e1f35285b5767281a88
Angel <48069264+velk20@users.noreply.github.com>
2024-08-14 16:45:29 +0300

    Gateaway

--- order-service/src/main/java/com/angel/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/angel/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.angel.microservices.order.service;
 
+import com.angel.microservices.order.client.InventoryClient;
 import com.angel.microservices.order.dto.OrderRequest;
 import com.angel.microservices.order.model.Order;
 import com.angel.microservices.order.repository.OrderRepository;
@@ -12,18 +13,26 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        //map OrderRequest to Order object
-        Order order = Order
-                .builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .price(orderRequest.price())
-                .skuCode(orderRequest.skuCode())
-                .quantity(orderRequest.quantity())
-                .build();
+        var inStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        //save to repository
-        orderRepository.save(order);
+        if (inStock) {
+            //map OrderRequest to Order object
+            Order order = Order
+                    .builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .price(orderRequest.price())
+                    .skuCode(orderRequest.skuCode())
+                    .quantity(orderRequest.quantity())
+                    .build();
+
+            //save to repository
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+
     }
 }
--- order-service/src/test/java/com/angel/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/angel/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.angel.microservices.order;
 
+import com.angel.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 
@@ -14,6 +16,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -41,6 +44,7 @@ class OrderServiceApplicationTests {
                 }
                 """;
 
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
 
 		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
