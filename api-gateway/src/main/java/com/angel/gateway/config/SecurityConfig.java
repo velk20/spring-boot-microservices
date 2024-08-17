132ed9f9e0cb69dd7656b1607f2147065205d1df
Angel <48069264+velk20@users.noreply.github.com>
2024-08-17 11:02:24 +0300

    Gateway open api has access to other servies

--- api-gateway/src/main/java/com/angel/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/angel/gateway/config/SecurityConfig.java
@@ -8,10 +8,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    private final String[] freeResourceUrls = {
+                                                "/swagger-ui.html",
+                                                "/swagger-ui/**",
+                                                "/v3/api-docs/**",
+                                                "/swagger-resources/**",
+                                                "/api-docs/**",
+                                                "/aggregate/**"};
 
     @Bean
     public SecurityFilterChain securityFilterChain( HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest()
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(freeResourceUrls)
+                        .permitAll()
+                        .anyRequest()
                         .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
